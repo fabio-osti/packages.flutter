8d17235ac5aefeabb8098a9f1e857087a42ffa55
Fabio Luiz Alvares Osti <fabio.osti@hotmail.com>
2022-05-18 00:18:40 -0300

    Null assertion removed for WidgetBinding.instance

--- packages/epub_view/example/lib/main.dart
+++ packages/epub_view/example/lib/main.dart
@@ -15,13 +15,13 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
   @override
   void initState() {
-    WidgetsBinding.instance!.addObserver(this);
+    WidgetsBinding.instance.addObserver(this);
     super.initState();
   }
 
   @override
   void dispose() {
-    WidgetsBinding.instance!.removeObserver(this);
+    WidgetsBinding.instance.removeObserver(this);
     super.dispose();
   }
 
@@ -31,7 +31,7 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
   }
 
   Brightness get platformBrightness =>
-      MediaQueryData.fromWindow(WidgetsBinding.instance!.window)
+      MediaQueryData.fromWindow(WidgetsBinding.instance.window)
           .platformBrightness;
 
   void _setSystemUIOverlayStyle() {
